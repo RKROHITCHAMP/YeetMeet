8efaa3694f6cd8d62e7ca1a05644ac0967765eab
ROHIT KUMAR <RK8826177@GMAIL.COM>
2021-09-04 10:24:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
-import os
+import os 
+
 class Config(object):
-	BOT_TOKEN = os.environ.get('BOT_TOKEN')
-	USERNAME = os.environ.get('USERNAME')
-	PASSWORD = os.environ.get('PASSWORD')
+
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '1983924143:AAHTaXhE7X7eMuk7VpqpSpyD2HzNGTZSMss') 
+
+    USERNAME = os.environ.get('USERNAME', 'vivek.yadav.ug20@nsut.ac.in') 
+
+    PASSWORD = os.environ.get('PASSWORD', 'breastfeed4U.')
